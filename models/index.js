c422ec4da5dac62b7a6e00689a6d67f2cead3b40
you know who <chorvickprocode@gmail.com>
2021-03-30 22:26:47 -0400

    completed models user post and comment

--- models/index.js
+++ models/index.js
@@ -1,14 +1,40 @@
 const User = require('./User');
-const Project = require('./Post');
+const Post = require('./Post');
+const Comment = require("./Comment");
 
+
+//associations user can have many posts  posts belong to user -- comments can belong to user and post, user can have many comments and post can have many comments
 User.hasMany(Post, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
+  foreignKey: "user_id",
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: "CASCADE",
 });
+
+Comment.belongsTo(User, {
+  foreignKey: "user_ud",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "pose_id",
+  onDelete: "CASCADE",
+});
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id"
+});
+
+
 
 module.exports = { User, Post };
 
