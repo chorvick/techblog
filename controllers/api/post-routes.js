7175bc62b58b8ae2c26852c86f5804ef63860d6c
you know who <chorvickprocode@gmail.com>
2021-04-01 00:37:28 -0400

    user and posting api routes added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -55,17 +55,68 @@ router.post("/", withAuth, (req, res) => {
 });
 
 
+/// check user posts
+
+router.get("/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+
+        },
+        attributes: ["id", "discription", "title", "created_at"],
+        include: [{
+            model: User,
+            attributes: ["username"],
+        },
+        {
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+            include: {
+                model: User,
+                attributes: ["username"],
+            }
 
 
+        }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "Sorry there is no post for that id" });
+                return;
 
+            }
+            res.json(dbPostData);
 
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 
+});
 
+// deleting a post............
 
+router.delete("/:id", withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
 
+    }).than(dbPostData => {
+        if (!dbPostData) {
 
+            res.status(404).json({ message: "No post was found with this id, Sorry" });
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.json(500).json(err);
 
-
+    });
+});
 
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,14 +1,117 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 
+// get users
+
+router.get("/", (req, res) => {
+    User.findAll({
+        attributes: { exclude: ["password"] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+});
+
+//// log out 
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+});
 
 
 
 
 
+router.put("/:id", (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user available with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+});
 
 
+/// delete
 
+router.delete("/:id", (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user w/ this id is available, sorry " });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(error);
+            res.status(500).json(err);
+        });
+});
+
+// getting user with id 
+
+router.get("/:id", (req, res) => {
+    User.findOne({
+        attributes: { exclude: ["password"] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ["id", "title", "description", "created_at"],
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "created_at"],
+                include: {
+                    model: Post,
+                    attributes: ["title"]
+                }
+            },
+
+
+        ]
+
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Sorry no user has this id number" });
+                return;
+
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
 
 
 
